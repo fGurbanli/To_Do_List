80bbee6aee6c23149b5468df7a0ef476afab599a
Furgan Gurbanli <f.qurbanli.2006@gmail.com>
2026-07-22 20:12:27 +0400

    functions initialized

--- main.c
+++ main.c
@@ -3,12 +3,34 @@
 #include <string.h>
 
 typedef struct Task {
-    char* task[];
-    char* date[];
+    char* name;
+    char* date;
 }tasks;
 
+int GetIntInput();
 
+void PrintMenu();
+void AddTask();
+void CompleteTask();
+void TaskList();
+void SearchTask();
+void EditTask();
 
 int main(void) {
 
+
+
+    return 0;
+}
+
+int GetIntInput() {
+    int input;
+    while (1) {
+        if (scanf("%d", &input) == 1 && input > 0) {
+            break;
+        }
+        while (getchar() != '\n');
+        printf("Enter a valid value!\n");
+    }
+    return input;
 }
